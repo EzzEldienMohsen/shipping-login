23de93ea9d0840311ddb04bc290a083833acabe6
Ezz dghedy <ezzmohsend@gmail.com>
2024-03-03 13:57:42 +0200

    inital fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
+import React from 'react';
+import FormRow from './components/FormRow';
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
 function App() {
+  const [values, setValues] = React.useState(initialValues);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-      <div className="avatar">
-        <div className="w-24 rounded-xl">
-          <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
-      </div>
-    </>
+    <form onSubmit={onSubmit}>
+      <h1>this is the title</h1>
+      {/* NAME FIELD */}
+
+      <FormRow
+        name="name"
+        type="text"
+        label="name"
+        value={values.name}
+        handleChange={handleChange}
+      />
+
+      {/* EMAIL */}
+      <FormRow
+        name="email"
+        type="email"
+        label="email"
+        value={values.email}
+        handleChange={handleChange}
+      />
+      {/* PASSWORD */}
+      <FormRow
+        name="password"
+        type="password"
+        label="password"
+        value={values.password}
+        handleChange={handleChange}
+      />
+      {/* SUBMIT BUTTON */}
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
   );
 }
 
--- src/components/FormRow.jsx
+++ src/components/FormRow.jsx
@@ -1,19 +1,17 @@
+/* eslint-disable react/prop-types */
 const FormRow = ({ name, label, type, value, handleChange }) => {
   return (
-    <div className="form-row">
-      <label htmlFor={name} className="form-label">
-        {label || name}
-      </label>
+    <div>
+      <label htmlFor={name}>{label || name}</label>
       <input
         type={type}
         name={name}
         id={name}
         value={value}
         onChange={handleChange}
-        className="form-input"
       />
     </div>
-  )
-}
+  );
+};
 
-export default FormRow
+export default FormRow;
